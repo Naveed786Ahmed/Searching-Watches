f17911edc8b58bbd72b2af9c0d01c8b433215985
Naveed786Ahmed <138322439+Naveed786Ahmed@users.noreply.github.com>
2024-02-24 22:54:37 +0500

    searching app

---
+++ pages/app.js
@@ -0,0 +1,321 @@
+
+let watches = {
+    Rolex: {
+        Submariner: {
+            name: "Rolex Submariner",
+            size: "40mm",
+            waterResistance: "300 meter",
+            movement: "Various options including automatic and chronograph movements",
+            price: "245,000 PKR",
+            pic: `https://w7.pngwing.com/pngs/461/109/png-transparent-rolex-submariner-rolex-datejust-rolex-gmt-master-ii-rolex-milgauss-rolex-watch-accessory-steel-platinum.png`
+        },
+
+        Daytano: {
+            name: "Rolex Daytano",
+            size: "40mm",
+            waterResistance: "100 meter",
+            movement: "Various options including automatic and chronograph movements",
+            price: "220,000 PKR",
+            pic: `https://w7.pngwing.com/pngs/230/236/png-transparent-rolex-daytona-rolex-submariner-rolex-oyster-perpetual-cosmograph-daytona-watch-rolex-bracelet-watch-accessory-gold.png`
+        },
+
+        OysterPerpetual: {
+            name: "Oyster Perpetual",
+            size: "37mm",
+            waterResistance: "200 meter",
+            movement: "Various options including automatic and chronograph movements",
+            price: "250,000 PKR",
+            pic: `https://i.pinimg.com/564x/43/be/7d/43be7dcf994481de68e634f6b78a3fad.jpg`
+        },
+
+        Datejust: {
+            name: "Rolex Datejust",
+            size: "41mm",
+            waterResistance: "100 meter",
+            movement: "Various options including automatic and chronograph movements",
+            price: "240,000 PKR",
+            pic: `https://firstclasstimepieces.com/cdn/shop/products/RolexDatejustIIGolden_FlutedMotifFlutedDial41mm126333_1800x1800.png?v=1670621670`
+        },
+
+        Explorer: {
+            name: "Rolex Explorer",
+            size: "35mm",
+            waterResistance: "100 meter",
+            movement: "Various options including automatic and chronograph movements",
+            price: "200,000 PKR",
+            pic: `https://w7.pngwing.com/pngs/478/981/png-transparent-rolex-submariner-bob-s-watches-rolex-oyster-perpetual-explorer-ii-rolex.png`
+        }
+    },
+
+    Hublot: {
+        BigBang: {
+            name: "Hublot Big Bang",
+            size: "41mm",
+            waterResistance: "100 meter",
+            movement: "Various options including automatic and chronograph movements",
+            price: "325,000 PKR",
+            pic: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTdd73UCO1xZs7094juhP_e98TePZtU5uPOWCEtoqWNA&s`
+
+        },
+
+        ClassicFusion: {
+            name: "Classic Fusion",
+            size: "38mm",
+            waterResistance: "300 meter",
+            movement: "Various options including automatic and chronograph movements",
+            price: "270,000 PKR",
+            pic: `https://www.hublot.com/sites/default/files/styles/boutique_hero_image_mobile/public/2023-02/Classic-Fusion-ceramic-king-gold-soldier-shot.png?itok=B9jUhk_I`
+        },
+
+        KingPower: {
+            name: "Hublot King Power",
+            size: "48mm",
+            waterResistance: "300 meter",
+            movement: "Various options including automatic and chronograph movements",
+            price: "400,000 PKR",
+            pic: `https://e7.pngegg.com/pngimages/845/109/png-clipart-hublot-king-power-chronograph-chronometer-watch-watch-bracelet-watch-accessory.png`
+        },
+
+        SpiritofBigBang: {
+            name: "Spirit of Big Bang",
+            size: "45mm",
+            waterResistance: "250 meter",
+            movement: "Various options including automatic and chronograph movements",
+            price: "380,000 PKR",
+            pic: `https://www.hublot.com/sites/default/files/styles/watch_item_desktop_1x_scale_no_crop_600_6000_/public/2023-01/645.QL_.5117.RX-spirit-of-big-bang-tourbillon-carbon-sky-blue-42-mm.png?itok=RbUjxzAk`
+        },
+
+        Masterpiece: {
+            name: "Hublot Masterpiece",
+            size: "40mm",
+            waterResistance: "100 meter",
+            movement: "Various options including automatic and chronograph movements",
+            price: "260,000 PKR",
+            pic: `https://askme.watch/sites/default/files/styles/watch_full_content/public/watch/images/540afb2f790e4_hublot-mp-02-key-of-time-titanium-902nx1179rx.png?itok=g1kkN-3-`
+        }
+    },
+
+    Tissot: {
+        LeLocle: {
+            name: "Tissot Le Locle",
+            size: "42mm",
+            waterResistance: "60 meter",
+            movement: "Automatic or quartz movement options available",
+            price: "65,000 PKR",
+            pic: `https://e7.pngegg.com/pngimages/817/443/png-clipart-round-black-tissot-1853-analog-watch-with-black-leather-strap-tissot-automatic-watch-chronometer-watch-water-resistant-mark-tissot-wristwatch-male-watch-black-watches-brown-black-thumbnail.png`
+        },
+
+        T_Touch: {
+            name: "Tissot T-Touch",
+            size: "42mm",
+            waterResistance: "60 meter",
+            movement: "Quartz movement with tactile functionality",
+            price: "70,000 PKR",
+            pic: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRktfDAq0uAvztIU7vAYeMmkT344eBJiuywqlytUi82AA&s`
+        },
+
+        PR100: {
+            name: "Tissot PR 100",
+            size: "38mm",
+            waterResistance: "60 meter",
+            movement: "Quartz movement",
+            price: "40,000 PKR",
+            pic: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUZDjSBlvsoKTx7wb9-AfKKITSTBWLo32ETgoLnceQLiGHKrjMvg6LwPzMyUrhmk9vvdI&usqp=CAU`
+        },
+
+        Carson: {
+            name: "Tissot Carson",
+            size: "40mm",
+            waterResistance: "60 meter",
+            movement: "Automatic or quartz movement options available",
+            price: "40,000 PKR",
+            pic: `https://iwc.com.pk/wp-content/uploads/2023/08/T122.410.11.033.00.jpg`
+        },
+
+        Chrono_XL: {
+            name: "Tissot Chrono XL",
+            size: "45mm",
+            waterResistance: "60 meter",
+            movement: "Quartz chronograph movement",
+            price: "60,000 PKR",
+            pic: `https://royalwrist.pk/wp-content/uploads/2023/06/1-117.png`
+        }
+    },
+
+    Daniel_Willingion: {
+        ClassicPetite: {
+            name: "Classic Petite",
+            size: "30mm",
+            waterResistance: "40 meter",
+            movement: "Japanese quartz movement",
+            price: "25,000 PKR",
+            pic: `https://my.danielwellington.com/cdn/shop/products/f83e0a252ca2e0a6fa29223860e03665be838c10.png?v=1678267027`
+        },
+
+        ClassicBristol: {
+            name: "Daniel Wellington Classic Bristol",
+            size: "37mm",
+            waterResistance: "40 meter",
+            movement: "Japanese quartz movement",
+            price: "30,000 PKR",
+            pic: `https://my.danielwellington.com/cdn/shop/products/23a8b83fad64c0a1c6d58007c8be10251cfa6b65.png?v=1678265960`
+        },
+
+        IconicLink: {
+            name: "Daniel Wellington Iconic Link",
+            size: "30mm",
+            waterResistance: "40 meter",
+            movement: "Japanese quartz movement",
+            price: "30,000 PKR",
+            pic: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnDyQvLdLlSytWV0bP9grLtNpujhto48Vu5s03DrsKTw&s`
+        },
+
+        ClassicStMawes: {
+            name: "Daniel Wellington Classic St. Mawes",
+            size: "38mm",
+            waterResistance: "40 meter",
+            movement: "Japanese quartz movement",
+            price: "30,000 PKR",
+            pic: `https://eu.danielwellington.com/cdn/shop/files/xmwifjcyb5aijxynetrc.png?v=1700819835&width=1500`
+        },
+
+        Dapper: {
+            name: "Daniel Wellington Dapper",
+            size: "35mm",
+            waterResistance: "40 meter",
+            movement: "Japanese quartz movement",
+            price: "30,000 PKR",
+            pic: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnDyQvLdLlSytWV0bP9grLtNpujhto48Vu5s03DrsKTw&s`
+        }
+    },
+
+    Sveston: {
+        Classic: {
+            name: "Sveston Classic",
+            size: "40mm",
+            waterResistance: "30 meter",
+            movement: "Quartz",
+            price: "22,000 PKR",
+            pic: `https://en-pk.svestonwatches.com/cdn/shop/files/SV-8164_Cam002M2.png?v=1697545162`
+        },
+
+        Sports: {
+            name: "Sveston Sports",
+            size: "42mm",
+            waterResistance: "50 meter",
+            movement: "Quartz",
+            price: "33,000 PKR",
+            pic: `https://en-pk.svestonwatches.com/cdn/shop/files/SV-11284_Cam002M4.png?v=1699859082`
+        },
+
+        Fashion: {
+            name: "Sveston Fashion",
+            size: "30mm",
+            waterResistance: "30 meter",
+            movement: "Quartz",
+            price: "28,000 PKR",
+            pic: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR81dDfribsijjif20mEEclE4ZfeU13ksO8UKu65-6B0w&s`
+        },
+
+        Elite: {
+            name: "Sveston Elite",
+            size: "42mm",
+            waterResistance: "50 meter",
+            movement: "Quartz",
+            price: "70,000 PKR",
+            pic: `https://en-pk.svestonwatches.com/cdn/shop/files/SV-7441_Cam002M1-01.png?v=1702880192`
+        },
+
+        Explorer: {
+            name: "Sveston Explorer",
+            size: "43mm",
+            waterResistance: "100 meter",
+            movement: "Quartz",
+            price: "80,000 PKR",
+            pic: `https://en-pk.svestonwatches.com/cdn/shop/products/SV-7405_Cam002M13.png?v=1695014544`
+        }
+    }
+}
+
+let main = document.getElementById("main");
+let brands = document.getElementById("brands");
+let models = document.getElementById("models");
+let detail = document.getElementById("watch-detail");
+
+brands.innerHTML = `<option>Select Brand Name</option>`;
+models.innerHTML = `<option>Select Model Name</option>`
+
+function readAllWatches(){
+    for (let key in watches) {
+        brands.innerHTML += `<option>${key}</option>`
+        for (let key1 in watches[key]) {
+            main.innerHTML += ` <div class="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
+            <div class="card mb-5" style="width: 18rem;">
+                <img src="${watches[key][key1].pic}" class="card-img card-img-top" alt="...">
+                <div class="card-body">
+                    <h3 class="card-title text-primary">${watches[key][key1].name}</h3>
+                    <h6 class="card-text">Case Size : ${watches[key][key1].size}</h6>
+                    <h6 class="card-text">Water Resistance : ${watches[key][key1].waterResistance}</h6>
+                    <h6 class="card-text">Price : ${watches[key][key1].price}</h6>
+                </div>
+            </div>
+        </div>
+    `
+        }
+    }
+}
+
+readAllWatches()
+
+function setModel() {
+    models.innerHTML = "";
+    models.innerHTML = `<option>Select Model Name</option>`
+    for (let key in watches[brands.value]) {
+        models.innerHTML += `<option>${key}</option>`
+    }
+}
+
+function searchWatch(){
+    main.style.display = "none";
+    detail.style.display = "flex"
+    
+    let watch = watches[brands.value][models.value];
+
+    detail.innerHTML = `<div class="card mb-3">
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img src="${watch.pic}" class="img-fluid rounded-start search-img" alt="..."  >
+      </div>
+      <div class="col-md-8">
+        <div class="card-body m-15">
+          <h2 class="card-title">${watch.name.toUpperCase()}</h2>
+          <hr>
+          <p class="card-text">Case Size : ${watch.size}</p>
+          <p class="card-text">Water Resistence : ${watch.waterResistance}</p>
+          <p class="card-text">Movement : ${watch.movement}</p>
+          <b><p class="card-text">Price : ${watch.price}</p></b>
+        
+
+        </div>
+      </div>
+    </div>
+  </div>`
+}
+
+function clearSearch(){
+    detail.style.display = "none";
+    main.style.display = "flex";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
